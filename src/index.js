193b155314bfbfbecf5a14cd7e6d444adf0bc0b7
vandu1979 <vandana_kothari@yahoo.com>
2023-01-08 23:19:38 -0700

    React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,222 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// child component
+// class Square extends React.Component {
+//     // TODO: remove the constructor
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.state = {
+//     //          value: null,
+//     //         }
+//     // }
+//     render() {
+//            // TODO: use onClick={this.props.onClick}
+//     // TODO: replace this.state.value with this.props.value
+//       return (
+//         <button 
+//         className="square"
+//          onClick={() => this.props.onClick()}
+//         >
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
+//Function Component===================
+  function Square(props){
+    return(
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+      <button 
+      className="square"
+       onClick={() => props.onClick()}
+      >
+        {props.value}
+      </button>
+    )
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //Parent component
+  // remove constructor from Board and display a list of past moves 
+  class Board extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         squares: Array(9).fill(null),
+    //         xIsNext: true,
+    //     }
+    // }
+   
+    renderSquare(i) {
+     
+      return ( 
+      <Square 
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
+      />
+      );
+    }
+  
+    render() {
+    
+      // const winner = calculateWinner(this.state.squares);
+      // let status;
+      // if (winner){
+      //   status = 'Winner:' + winner;
+      // } else {
+      //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      // }
+
+      // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      // const status = 'Next player: X';
+  
+      return (
+        <div>
+          {/* <div className="status">{status}</div> */}
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        history: [{ squares: Array(9).fill(null)}],
+        xIsNext: true,
+        // add stepNumber to the Game component’s state to indicate which step we’re currently viewing.
+        stepNumber: 0,
+      };
+    }
+    handleClick(i) {
+      const history = this.state.history.slice(0,this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]){
+          return;
+        }
+        
+        squares[i] = this.state.xIsNext ? 'X': 'O';
+        this.setState({
+          history: history.concat([{
+            squares: squares,
+          }]),
+          stepNumber: history.length,
+          xIsNext: !this.state.xIsNext,
+        });
+    }
+    // Next, we’ll define the jumpTo method in Game to update that stepNumber. We also set xIsNext to true if the number that we’re changing stepNumber to is even:
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      })
+    }
+
+    render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+      // Let’s map over the history in the Game’s render method:
+      const moves = history.map((step, move) => {
+        const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+          
+        </div>
+      );
+    }
+  }
+ 
+  
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  // history = [
+  //   // Before first move
+  //   {
+  //     squares: [
+  //       null, null, null,
+  //       null, null, null,
+  //       null, null, null,
+  //     ]
+  //   },
+  //   // After first move
+  //   {
+  //     squares: [
+  //       null, null, null,
+  //       null, 'X', null,
+  //       null, null, null,
+  //     ]
+  //   },
+  //   // After second move
+  //   {
+  //     squares: [
+  //       null, null, null,
+  //       null, 'X', null,
+  //       null, null, 'O',
+  //     ]
+  //   },
+  //   // ...
+  // ]
